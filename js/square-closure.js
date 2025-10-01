76db2be1efcbea0473dc1dee992efee7278bc449
prapaudel-ctr <prapaudel@algomau.ca>
2025-10-01 16:36:48 -0400

    Update square-closure.js

--- js/square-closure.js
+++ js/square-closure.js
@@ -1,6 +1,4 @@
-// This is the outer function that will form the closure
 function createSquareFunction() {
-    // This is the inner function that "closes over" the num variable
     function square(num) {
         return num * num;
     }
